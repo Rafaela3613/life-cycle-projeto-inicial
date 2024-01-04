f728cbcb813714d9e4406a8232a93e14e033c477
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-04 15:56:22 -0300

    Alimentando a lista

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -30,6 +30,7 @@ export class ListaDeCompraService {
   adicionarItemNaLista(nomeDoItem: string){
     const item = this.criarItem(nomeDoItem)
     this.listaDeCompra.push(item);
+    this.atualizarLocalStorage();
   }
 
   editarItemDaLista(itemAntigo: Item, nomeEditadoDoItem: string){
@@ -41,6 +42,11 @@ export class ListaDeCompraService {
     }
     const id = itemAntigo.id;
     this.listaDeCompra.splice(Number(id)-1, 1, itemEditado);
+    this.atualizarLocalStorage();
+  }
+
+  atualizarLocalStorage() {
+    localStorage.setItem('itens', JSON.stringify(this.listaDeCompra));
   }
 
 }

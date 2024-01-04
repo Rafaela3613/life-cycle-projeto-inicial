abd6b6d4a7e0cbb2ba9d0c35c45fc9df652a8719
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-03 23:46:28 -0300

    incluindo a exibicao de itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+import { Item } from './interfaces/iItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app-lista-de-compras';
+//código omitido
 
-  constructor() { }
+export class AppComponent implements OnInit {
+  title = 'app-lista-de-compras';
+  listaDeCompra! : Array<Item>
+  constructor(private listaService: ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompra);
+  }
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
+
 export class ItemComponent implements OnInit {
+  @Input() item!: Item;
 
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
-
   ngOnInit(): void { }
-
 }
+

cb500ea766cff6534c8662285ba803806a8241d7
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-01-04 10:15:37 -0300

    Adicionando novos itens à lista

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputComponent implements OnInit {
 
+  valorItem! : string
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  adicionarItem(){
+    console.log(this.valorItem);
+  }
 }
